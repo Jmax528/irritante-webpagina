d17b1150f981a08d911b36c68555a849ed321cfd
Jmax528 <45967298+Jmax528@users.noreply.github.com>
2023-12-13 13:22:37 +0100

    Add files via upload

---
+++ irritante web/js/annoy.js
@@ -0,0 +1,32 @@
+//Event listener, gets the id of the elements
+document.addEventListener('DOMContentLoaded', function () {
+    let button = document.getElementById('button');
+    let grid = document.getElementById('grid');
+    let item2 = document.getElementById('item2');
+    let item3 = document.getElementById('item3');
+    let text = document.getElementById('text');
+    let text2 = document.getElementById('text2');
+    let h2 = document.getElementById('h2');
+    let phone = document.getElementById('phone');
+    let maxor = document.getElementById('maxor');
+
+//toggles the css class to the html element when the button is clicked, starting and stopping the animations
+    button.addEventListener('click', function () {
+        grid.classList.toggle('animate');
+        phone.classList.toggle('rotatePhone');
+        item2.classList.toggle('animateTwo');
+        item3.classList.toggle('animateThree');
+        h2.classList.toggle('rotate');
+        text.classList.toggle('rotateTwo');
+        text2.classList.toggle('rotateTwo');
+        button.classList.toggle('animateThree');
+        maxor.classList.toggle('show', phone.classList.contains('rotatePhone'));
+    });
+});
+
+
+
+
+
+
+
---
+++ irritante web/js/player.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#button').click(function(event) {
+        let video = $('#maxor');
+        let autoplayValue = video.attr('src').includes('autoplay=1') ? '0' : '1';
+        video.attr('src', `${video.attr('src').split('&autoplay=')[0]}&autoplay=${autoplayValue}`);
+        console.log(autoplayValue);
+    });
+});
+
+// Function, on click of the button it plays the video, on a second click it stops the video
+// autoplayValue sets the value to either a 0 or 1 where the video attribute includes 'autoplay=1'
+// gets the source of the url, .split('&autoplay=')[0]: Takes the URL and removes everything after '&autoplay='
+// after which the video source url is updated (see; 1)
